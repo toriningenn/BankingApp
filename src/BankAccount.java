65d5aeb86a67ae735bfa5724f8a6ddb52f22b4bd
toriningenn <annamihailova72@yandex.com>
2020-10-17 20:13:14 +0200

    test

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -51,39 +51,35 @@ public class BankAccount {
             System.out.println("========================= ");
             option = scanner.next().charAt(0);
             System.out.println("\n");
-            switch(option) {
-                case 'A':
+            switch (option) {
+                case 'A' -> {
                     System.out.println("========================= ");
                     System.out.println("Balance: " + balance);
                     System.out.println("========================= ");
                     System.out.println("\n");
-                    break;
-                case 'B':
+                }
+                case 'B' -> {
                     System.out.println("========================= ");
                     System.out.println("Enter an amount to deposit:");
                     System.out.println("========================= ");
                     deposit(scanner.nextInt());
                     System.out.println("\n");
-                    break;
-                case 'C':
+                }
+                case 'C' -> {
                     System.out.println("========================= ");
                     System.out.println("Enter an amount to withdraw:");
                     System.out.println("========================= ");
                     withdraw(scanner.nextInt());
                     System.out.println("\n");
-                    break;
-                case 'D':
+                }
+                case 'D' -> {
                     System.out.println("========================= ");
                     getPrTransaction();
                     System.out.println("========================= ");
                     System.out.println("\n");
-                    break;
-                case 'E':
-                    System.out.println("*************************");
-                    break;
-                default:
-                    System.out.println("INVALID OPTION");
-                    break;
+                }
+                case 'E' -> System.out.println("*************************");
+                default -> System.out.println("INVALID OPTION");
             }
         } while (option != 'E');
 
